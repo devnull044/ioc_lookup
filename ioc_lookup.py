30d17ad212cdaa431162a785a932d4b3f00d570c
devnull044 <43389363+devnull044@users.noreply.github.com>
2020-02-18 15:42:20 -0600

    Update ioc_lookup.py
    added vt and pulsedive to check_url function

--- ioc_lookup.py
+++ ioc_lookup.py
@@ -4,6 +4,7 @@ import validators
 import argparse
 import os
 import pulsedive
+import base64
 
 parser = argparse.ArgumentParser()
 parser.add_argument("ioc", help="Enter the ioc youd like to query")
@@ -13,6 +14,7 @@ ioc = args.ioc.strip()
 print('Searching the IOC %s...' % ioc)
 
 keys = {}
+headers = {}
 #read apis from config file
 with open("keys.cfg", "r") as keys_c:
     next(keys_c)
@@ -28,7 +30,6 @@ else:
     
 #check_ip
 def check_ip(ioc):
-    headers = {}
     #vt check ip
     headers['x-apikey'] = keys['vt']
     vt_ip_api = "https://www.virustotal.com/api/v3/ip_addresses/%s" % ioc
@@ -54,6 +55,34 @@ def check_ip(ioc):
         if 'Indicator not found' in str(e):
             print('Indicator not found')
             #maybe add option to upload ioc to pulsedive
+
+#check_url
+def check_url(ioc):
+    #vt
+    headers['x-apikey'] = keys['vt']
+    vt_url_api = "https://www.virustotal.com/api/v3/urls/%s" % base64.urlsafe_b64encode(ioc.encode()).strip(b'=').decode()
+    vt_url = requests.get(url=vt_url_api,headers=headers)
+    vt_data = vt_url.json()
+    vtrep = vt_data['data']['attributes']['reputation']
+    print('--VirusTotal--')
+    print('Reputation: %s\n' % vtrep)
+    
+    #pulsedive
+    print('--PulseDive--')
+    try:
+        ind = pud.indicator(value=ioc)
+        if ind['risk'] == 'none':
+            print('Verdict Unknown')
+        else:
+            print(ind['risk'])        
+    except pulsedive.exceptions.PulsediveException as e:
+        if 'Indicator not found' in str(e):
+            print('Indicator not found')
+            #maybe add option to upload ioc to pulsedive
+    exit()
+
+def check_domain(ioc):
+        print('it bad!')
 #detect ioc, check if hash,ip, domain, or url
 def check_ioc(ioc):
     pass
@@ -65,7 +94,7 @@ def check_ioc(ioc):
     elif validators.domain(ioc):
         check_domain(ioc)
     else:
-        print("The IOC:'%s' cannot be search. The script only searches IPs, hashes, URLs, and domains" % ioc)
+        print("The IOC:'%s' cannot be searched. The script only searches IPs, hashes, URLs, and domains" % ioc)
         exit()
 
 check_ioc(ioc)
